e1590c4143e3d363134d994cefe832d75ca7a2f9
Nurmukhammad1 <90842370+Nurmukhammad1@users.noreply.github.com>
2024-08-25 21:12:55 +0500

    adding react-resizable to MainPage

--- src/pages/security/LoginPage.tsx
+++ src/pages/security/LoginPage.tsx
@@ -1,25 +1,31 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useToken } from "../../hooks/useToken";
 import axios from "axios";
-import { useUser } from "../../hooks/useUser";
+import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
 
-const LoginPage: React.FC = () =>  {
+
+const LoginPage: React.FC = () => {
   const [emailValue, setEmailValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [token, setToken] = useToken();
   const navigate = useNavigate(); 
 
   const onLogInClicked = async () => {
-    axios.post('http://localhost:8080/login',{}, { headers:  {
-      "username":emailValue,
-      "password":passwordValue,
-   }}).then((response) => {
-    setToken(response.headers.getAuthorization());
-    navigate("/");
-   })
+    try {
+      const response = await axios.post('http://localhost:8080/login', {}, {
+        headers:  {
+          "username": emailValue,
+          "password": passwordValue,
+        }
+      });
+      setToken(response.headers.getAuthorization());
+      navigate("/");
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+    }
   };
 
   const onForgotPasswordClicked = () => {
@@ -31,47 +37,57 @@ const LoginPage: React.FC = () =>  {
   };
 
   return (
-		<div className='login'>
-			<div className='login-container'>
-				<h1 className='login-title'>Log In</h1>
-				{errorMessage && <div>{errorMessage}</div>}
-				<input
-					value={emailValue}
-					onChange={(e) => setEmailValue(e.target.value)}
-					placeholder='user@mailbox.xyz'
-					className='emailInput'
-				/>
-				<input
-					value={passwordValue}
-					type='password'
-					onChange={(e) => setPasswordValue(e.target.value)}
-					placeholder='password'
-					className='passwordInput'
-				/>
-				<hr style={{ marginBottom: '20px' }} />
-				<div className='authorizationButtons'>
-					<button 
-          disabled={!emailValue || !passwordValue} 
-          onClick={onLogInClicked}
-          className="loginButton"
+    <div className='login'>
+      <div className='login-container'>
+        <h1 className='login-title'>Log In</h1>
+        {errorMessage && <div>{errorMessage}</div>}
+        <input
+          value={emailValue}
+          onChange={(e) => setEmailValue(e.target.value)}
+          placeholder='Enter your login'
+          className='emailInput'
+        />
+        <div className='passwordInputContainer'>
+          <input
+            value={passwordValue}
+            type={showPassword ? 'text' : 'password'}
+            onChange={(e) => setPasswordValue(e.target.value)}
+            placeholder='password'
+            className='passwordInput'
+          />
+          <button 
+            type="button" 
+            onClick={() => setShowPassword(!showPassword)} 
+            className='showPasswordButton'
           >
-						Log In
-					</button>
-					<button 
-          onClick={onForgotPasswordClicked}
-          className="forgotPasswordButton"
+            {showPassword ? <EyeInvisibleOutlined /> : <EyeOutlined />}
+          </button>
+        </div>
+        <hr style={{ marginBottom: '20px' }} />
+        <div className='authorizationButtons'>
+          <button 
+            disabled={!emailValue || !passwordValue} 
+            onClick={onLogInClicked}
+            className="loginButton"
+          >
+            Log In
+          </button>
+          <button 
+            onClick={onForgotPasswordClicked}
+            className="forgotPasswordButton"
           >
             Forgot password?
           </button>
-					<button 
-          onClick={onSignUpClicked}
-          className="signUpButton">
+          <button 
+            onClick={onSignUpClicked}
+            className="signUpButton"
+          >
             Sign Up
-            </button>
-				</div>
-			</div>
-		</div>
-	);
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
--- src/pages/security/SignUpPage.tsx
+++ src/pages/security/SignUpPage.tsx
@@ -1,27 +1,31 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {useToken} from "../../hooks/useToken";
 import axios from "axios";
+import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
+
 
 const SignUpPage: React.FC = () => {
   const [emailValue, setEmailValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
   const [confirmPasswordValue, setConfirmPasswordValue] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
   const onSignUpClicked = async () => {
-    const response = await axios.post('http://localhost:8080/sign-up',
-      {
-        "username":emailValue,
-        "password":passwordValue
-     }
-    )
-    if (response.data === "OK"){
-      // TODO show successfull user registration message and then propose to go to login page
-    } else {
-      //Propose to chose another username and propose otherwise
+    try {
+      const response = await axios.post('http://localhost:8080/sign-up', {
+        "username": emailValue,
+        "password": passwordValue
+      });
+      if (response.data === "OK") {
+        // TODO: show successful user registration message and then propose to go to login page
+      } else {
+        // Propose to choose another username and propose otherwise
+      }
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
     }
   };
 
@@ -29,50 +33,65 @@ const SignUpPage: React.FC = () => {
     navigate("/login");
   };
 
-
   return (
     <div className="signin">
       <div className="signin-container">
-      <h1 className="login-title">Sign Up</h1>
-      {errorMessage && <div>{errorMessage}</div>}
-      <input
-        value={emailValue}
-        onChange={(e) => setEmailValue(e.target.value)}
-        placeholder="user@mailbox.xyz"
-        className="emailInput"
-      />
-      <input
-        value={passwordValue}
-        type="password"
-        onChange={(e) => setPasswordValue(e.target.value)}
-        placeholder="password"
-        className="passwordInput"
-      />
-      <input
-        value={confirmPasswordValue}
-        type="password"
-        onChange={(e) => setConfirmPasswordValue(e.target.value)}
-        placeholder="password"
-        className="passwordInput"
-      />
-      <hr style={{ marginBottom: "20px" }}/>
-      <div className='authorizationButtons'>
-      <button
-        disabled={
-          !emailValue ||
-          !passwordValue ||
-          passwordValue !== confirmPasswordValue
-        }
-        onClick={onSignUpClicked}
-        className="loginButton"
-      >
-        Sign up
-      </button>
-      <button onClick={onLogInClicked} className="loginButton">Back to Log in</button>
+        <h1 className="login-title">Sign Up</h1>
+        {errorMessage && <div>{errorMessage}</div>}
+        <input
+          value={emailValue}
+          onChange={(e) => setEmailValue(e.target.value)}
+          placeholder="Enter your login"
+          className="emailInput"
+        />
+        <div className='passwordInputContainer'>
+          <input
+            value={passwordValue}
+            type={showPassword ? 'text' : 'password'}
+            onChange={(e) => setPasswordValue(e.target.value)}
+            placeholder="Enter a password"
+            className="passwordInput"
+          />
+          <button 
+            type="button" 
+            onClick={() => setShowPassword(!showPassword)} 
+            className='showPasswordButton'
+          >
+            {showPassword ? <EyeInvisibleOutlined /> : <EyeOutlined />}
+          </button>
+        </div>
+        <div className='passwordInputContainer'>
+          <input
+            value={confirmPasswordValue}
+            type={showConfirmPassword ? 'text' : 'password'}
+            onChange={(e) => setConfirmPasswordValue(e.target.value)}
+            placeholder="Confirme password"
+            className="passwordInput"
+          />
+          <button 
+            type="button" 
+            onClick={() => setShowConfirmPassword(!showConfirmPassword)} 
+            className='showPasswordButton'
+          >
+            {showConfirmPassword ? <EyeInvisibleOutlined /> : <EyeOutlined />}
+          </button>
+        </div>
+        <hr style={{ marginBottom: "20px" }}/>
+        <div className='authorizationButtons'>
+          <button
+            disabled={
+              !emailValue ||
+              !passwordValue ||
+              passwordValue !== confirmPasswordValue
+            }
+            onClick={onSignUpClicked}
+            className="loginButton"
+          >
+            Sign Up
+          </button>
+          <button onClick={onLogInClicked} className="loginButton">Back to Log in</button>
+        </div>
       </div>
-    
-      </div>
-     
     </div>
   );
 };
